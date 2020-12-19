3367983f431f55d2d35993cde9fb15cbcbfa27ef
Tatana <burmatova@inbox.ru>
2020-12-19 19:27:41 +0300

    added some invalid tests

--- src/test/java/ru/netology/CardOrderFormTestSelenium.java
+++ src/test/java/ru/netology/CardOrderFormTestSelenium.java
@@ -37,7 +37,7 @@ public class CardOrderFormTestSelenium {
     }
 
     @Test
-    void shouldSubmitRequest() {
+    void shouldSubmitRequestIfFieldsAreFilledCorrect() {
         driver.get("http://localhost:9999");
         driver.findElement(By.cssSelector("[type='text']")).sendKeys("Татьяна");
         driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+79990000000");
@@ -46,7 +46,66 @@ public class CardOrderFormTestSelenium {
 
         String actualMassage = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actualMassage.trim());
-
     }
+
+    @Test
+    void shouldNotSubmitRequestIfTextFieldIsEmpty() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[type='text']")).sendKeys("");
+        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+79990000000");
+        driver.findElement(By.cssSelector("[class='checkbox__text']")).click();
+        driver.findElement(By.cssSelector("[type='button']")).click();
+
+        String actualMassage = driver.findElement(By.cssSelector("[class='input__sub']")).getText();
+        assertEquals("Поле обязательно для заполнения", actualMassage.trim());
+    }
+
+    @Test
+    void shouldNotSubmitRequestIfTextFieldIsInvalid() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[type='text']")).sendKeys("jdhfh");
+        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+79990000000");
+        driver.findElement(By.cssSelector("[class='checkbox__text']")).click();
+        driver.findElement(By.cssSelector("[type='button']")).click();
+
+        String actualMassage = driver.findElement(By.cssSelector("[class='input__sub']")).getText();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", actualMassage.trim());
+    }
+
+//    @Test
+//    void shouldNotSubmitRequestIfTextFieldIsInvalid2() {
+//        driver.get("http://localhost:9999");
+//        driver.findElement(By.cssSelector("[type='text']")).sendKeys("ывалоыр");
+//        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+79990000000");
+//        driver.findElement(By.cssSelector("[class='checkbox__text']")).click();
+//        driver.findElement(By.cssSelector("[type='button']")).click();
+//
+//        String actualMassage = driver.findElement(By.cssSelector("span[class='input__sub']")).getText();
+//        assertEquals("Укажите точно как в паспорте", actualMassage.trim());
+//    }
+//
+//    @Test
+//    void shouldNotSubmitRequestIfTelFieldIsEmpty() {
+//        driver.get("http://localhost:9999");
+//        driver.findElement(By.cssSelector("[type='text']")).sendKeys("Татьяна");
+//        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("");
+//        driver.findElement(By.cssSelector("[class='checkbox__text']")).click();
+//        driver.findElement(By.cssSelector("[type='button']")).click();
+//
+//        String actualMassage = driver.findElement(By.cssSelector("[class='input__sub']")).getText();
+//        assertEquals("Поле обязательно для заполнения", actualMassage.trim());
+//    }
+//
+//    @Test
+//    void shouldNotSubmitRequestIfiTelFieldIsInvalid() {
+//        driver.get("http://localhost:9999");
+//        driver.findElement(By.cssSelector("[type='text']")).sendKeys("Татьяна");
+//        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("764758");
+//        driver.findElement(By.cssSelector("[class='checkbox__text']")).click();
+//        driver.findElement(By.cssSelector("[type='button']")).click();
+//
+//        String actualMassage = driver.findElement(By.cssSelector("[class='input__sub']")).getText();
+//        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", actualMassage.trim());
+//    }
 
 }
